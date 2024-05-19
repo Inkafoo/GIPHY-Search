75cdc26cc22c6bfaa88b47dd4ea6d4fcaee1e72d
Karol <karol.furtak24@gmail.com>
2024-05-19 12:37:20 +0200

    add entrypoint for hilt

--- app/src/main/java/com/giphy/searchapp/MainActivity.kt
+++ app/src/main/java/com/giphy/searchapp/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.giphy.sdk.ui.Giphy
 import com.giphy.searchapp.ui.theme.GIPHYSearchAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Giphy.configure(this, Helper.API_KEY)
+
         setContent {
             GIPHYSearchAppTheme {
                 // A surface container using the 'background' color from the theme
